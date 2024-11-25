f54c9038a934660504c017fad3479700100d2af0
danyshan <153415527+danyshan@users.noreply.github.com>
2024-11-25 15:48:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || '.' ,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=DypVjSRD#zObc7a_hyV2uAtjP1p8b25j1mCiQemDl2ntly654BAg' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '#' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
