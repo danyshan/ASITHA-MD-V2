fee188551d1c65f869f415e9915bf5818fce7da2
danyshan <153415527+danyshan@users.noreply.github.com>
2024-12-08 15:27:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,11 +4,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XvBFiAyC#Q8QsdW7EpKOcCcrpLS8F5TGNRfawLYh_kty1PsVN3Fs' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=iiRGmZjA#Bj9VH4hsyhtd9INpWJMkpsJXI45njvmbb73aioarNUY' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '#' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
-MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
+MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
 AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "false" : process.env.AUTO_REPLY,
 AUTO_STICKER: process.env.AUTO_STICKER === undefined ? "false" : process.env.AUTO_STICKER,
